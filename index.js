b6f63df87efe72090870f0ae8b293e0bca4ca0d8
Gokul Ram <gokulram2010418@ssn.edu.in>
2023-04-07 15:37:57 +0530

    added all projects

--- index.js
+++ index.js
@@ -1,3 +1,182 @@
+projectsList = [
+  {
+    title: 'Projects I worked on as an Intern:',
+    projects: [
+      {
+        title: 'Coronasafe CARE',
+        description:
+          'An open source integrated tele-triage+bed allocation+patient shifting tool for implementing a decentralised pandemic management strategy.',
+        image:
+          'https://res.cloudinary.com/di4swkq4j/image/upload/v1680853911/Screenshot_from_2023-04-07_13-21-15_fi2dun.png',
+        url: 'https://care.coronasafe.in/',
+        github: 'https://github.com/coronasafe/care_fe',
+        badges: [
+          { name: 'React', color: 'bg-blue-400' },
+          { name: 'Typescript', color: 'bg-blue-600' },
+          { name: 'Django', color: 'bg-green-600' },
+          { name: 'Tailwind', color: 'bg-sky-400' },
+        ],
+      },
+      {
+        title: 'Ticketh.io',
+        description:
+          'TickEth is an NFT ticketing marketplace that allows event organisers and ticket issuers to list and sell their tickets.',
+        image:
+          'https://res.cloudinary.com/di4swkq4j/image/upload/v1680854428/Screenshot_from_2023-04-07_13-30-09_vnwcgw.png',
+        url: 'https://ticketh.io',
+        github: 'https://github.com/ticketh-io',
+        badges: [
+          { name: 'Next JS', color: 'bg-gray-800' },
+          { name: 'Express', color: 'bg-green-600' },
+          { name: 'Tailwind', color: 'bg-sky-400' },
+          { name: 'Mongo DB', color: 'bg-green-500' },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Projects I worked on at college:',
+    projects: [
+      {
+        title: 'GPACalc',
+        description:
+          'Create your own calculator and share the link with friends. Say goodbye to manual calculations and hello to stress-free GPA calculating!',
+        image:
+          'https://res.cloudinary.com/di4swkq4j/image/upload/v1680855590/Screenshot_from_2023-04-07_13-47-53_etezy7.png',
+        url: 'https://gpacalc.vercel.app/',
+        github: 'https://github.com/GokulramGHV/GPACalc',
+        badges: [
+          { name: 'Next JS', color: 'bg-gray-800' },
+          { name: 'Express', color: 'bg-green-600' },
+          { name: 'Tailwind', color: 'bg-sky-400' },
+        ],
+      },
+      {
+        title: 'Procode Website',
+        description:
+          'Website of the official coding club of IT Department of SSN College of Engineering, Chennai.',
+        image:
+          'https://res.cloudinary.com/di4swkq4j/image/upload/v1680857537/Screenshot_from_2023-04-07_14-21-50_ibnbud.png',
+        url: 'https://procode-it.vercel.app/',
+        github: 'https://github.com/procode-it-ssn/Procode-website',
+        badges: [
+          { name: 'Next JS', color: 'bg-gray-700' },
+          { name: 'Tailwind', color: 'bg-sky-400' },
+        ],
+      },
+      {
+        title: 'The Attendance App',
+        description:
+          'Create your own calculator and share the link with friends. Say goodbye to manual calculations and hello to stress-free GPA calculating!',
+        image:
+          'https://res.cloudinary.com/di4swkq4j/image/upload/v1680856001/Screenshot_from_2023-04-07_13-56-14_td9hhf.png',
+        url: 'https://attendance-app-nu.vercel.app/',
+        github: 'https://github.com/GokulramGHV/Attendance-App-frontend',
+        badges: [
+          { name: 'Next JS', color: 'bg-gray-800' },
+          { name: 'Django', color: 'bg-green-600' },
+          { name: 'Tailwind', color: 'bg-sky-400' },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Projects I worked on when learning development:',
+    projects: [
+      {
+        title: 'Kanban Task Manager',
+        description:
+          'An agile project management tool designed to help visualize work, limit work-in-progress, and maximize efficiency.',
+        image:
+          'https://user-images.githubusercontent.com/40627011/175811986-2d4bf367-ce2f-4aa8-949b-312a4c83150d.png',
+        url: 'https://taskapp-301.netlify.app/',
+        github: 'https://github.com/GokulramGHV/Kanban-Tasks-Application',
+        badges: [
+          { name: 'React', color: 'bg-blue-400' },
+          { name: 'Typescript', color: 'bg-blue-600' },
+          { name: 'Django', color: 'bg-green-600' },
+          { name: 'Tailwind', color: 'bg-sky-400' },
+        ],
+      },
+      {
+        title: 'React Forms',
+        description:
+          'A simple GoogleForm/Typeform clone that can create forms and share the link with others.',
+        image:
+          'https://camo.githubusercontent.com/a1877f48882c38cf0f37865eb092304c82afae152164b19d2e2863fab4ff48b0/68747470733a2f2f7777772e707570696c66697273742e7363686f6f6c2f6d61726b646f776e5f6174746163686d656e74732f343633332f6c732d536f6b487754504d746f59786e595667486267',
+        url: 'https://singular-marshmallow-ec82c9.netlify.app/',
+        github: 'https://github.com/GokulramGHV/WD301-React-TypeScript-Course',
+        badges: [
+          { name: 'React', color: 'bg-blue-400' },
+          { name: 'Typescript', color: 'bg-blue-600' },
+          { name: 'Tailwind', color: 'bg-sky-400' },
+        ],
+      },
+      {
+        title: 'Minimal Task Manager',
+        description:
+          'A simple and minimal web app that manages and prioritizes your tasks!',
+        image:
+          'https://raw.githubusercontent.com/GokulramGHV/gokulramghv.github.io/site-v1/assets/task_manager.png',
+        url: 'https://task-manager-gokul.herokuapp.com/',
+        github: 'https://github.com/GokulramGHV/Task-Manager-GDC',
+        badges: [
+          { name: 'React', color: 'bg-blue-400' },
+          { name: 'Typescript', color: 'bg-blue-600' },
+          { name: 'Django', color: 'bg-green-600' },
+          { name: 'Tailwind', color: 'bg-sky-400' },
+        ],
+      },
+    ],
+  },
+  {
+    title: 'Projects I worked on when I was bored:',
+    projects: [
+      {
+        title: 'Quick Note',
+        description:
+          'A simple web app that saves notes with markdown functionality made using python flask.',
+        image:
+          'https://res.cloudinary.com/di4swkq4j/image/upload/v1680859507/Screenshot_from_2023-04-07_14-54-30_zqpvx2.png',
+        url: 'https://quick-note-web.herokuapp.com/',
+        github: 'https://github.com/GokulramGHV/Quick-Note',
+        badges: [
+          { name: 'Flask', color: 'bg-gray-700' },
+          { name: 'Python', color: 'bg-blue-500' },
+          { name: 'Bootstrap', color: 'bg-indigo-600' },
+        ],
+      },
+      {
+        title: 'Wordle CLI',
+        description:
+          'Standard Wordle Clone in command line interface (CLI) using the python rich module.',
+        image:
+          'https://raw.githubusercontent.com/GokulramGHV/gokulramghv.github.io/site-v1/assets/wordle_clone-2.png',
+        url: 'https://replit.com/@GokulramGHV/WordleCLI',
+        github: 'https://github.com/GokulramGHV/Wordle-Clone',
+        badges: [
+          { name: 'Python', color: 'bg-blue-500' },
+          { name: 'Rich', color: 'bg-red-500' },
+        ],
+      },
+      {
+        title: 'Graph Visualizer',
+        description:
+          'Using this web application you can visualize graphs and some of the path finding algorithms on them.',
+        image:
+          'https://user-images.githubusercontent.com/40627011/175812532-1500113f-148b-47ab-aee4-746f59e28b2d.png',
+        url: 'https://gokulramghv.github.io/Urban-Drainage-Management/',
+        github: 'https://github.com/GokulramGHV/Urban-Drainage-Management',
+        badges: [
+          { name: 'JQuery', color: 'bg-orange-500' },
+          { name: 'Vis JS', color: 'bg-green-500' },
+          { name: 'Tailwind', color: 'bg-sky-400' },
+        ],
+      },
+    ],
+  },
+];
+
 let darkMode = false;
 document.getElementById('sun').style.display = 'none';
 function toggle() {
@@ -32,7 +211,6 @@ function createVideoElement(url, id, hidden) {
   else video.classList.add('fixed', 'top-0', 'left-0', 'z-0');
   video.preload = 'auto';
   video.src = url;
-  // 'https://res.cloudinary.com/di4swkq4j/video/upload/v1679815224/light_gk1exr.mp4';
   video.id = id;
   document.getElementsByTagName('body')[0].appendChild(video);
 }
@@ -66,15 +244,17 @@ let viewProjects = false;
 document.getElementById('projects').style.display = 'none';
 function setViewProjects() {
   viewProjects = !viewProjects;
+  const spline_viewer = document.getElementById('spline-viewer');
   if (viewProjects) {
     document.getElementById('projects').style.display = 'flex';
     document.getElementById('projects').classList.add('elementToPopIn');
+    window.scrollTo(0, 0);
     setTimeout(() => {
       document.getElementById('home').style.display = 'none';
-      document.getElementById('spline-viewer').style.display = 'none';
+      if (spline_viewer) spline_viewer.style.display = 'none';
     }, 500);
   } else {
-    document.getElementById('spline-viewer').style.display = 'block';
+    if (spline_viewer) spline_viewer.style.display = 'block';
     document.getElementById('home').style.display = 'flex';
     document.getElementById('projects').classList.remove('elementToPopIn');
     document.getElementById('projects').classList.add('elementToPopOut');
@@ -83,3 +263,105 @@ function setViewProjects() {
     }, 500);
   }
 }
+
+function renderProjectSection(projectSection) {
+  let htmlText = `<div class="my-6">
+  <h2 class="md:text-2xl text-xl font-bold">
+    ${projectSection.title}
+  </h2>
+  <div class="grid lg:grid-cols-3 md:grid-cols-2 mt-6 gap-6 lg:gap-10">
+    `;
+  projectSection.projects.forEach((project) => {
+    htmlText += `<div
+    class="card hover:shadow-xl dark:bg-slate-700 hover:scale-[102%]"
+  >
+    <a href="${project.url}" target="_blank">
+      <img
+        class="w-full h-64 object-cover"
+        src="${project.image}"
+        alt="stew"
+      />
+    </a>
+
+    <div class="m-4 flex flex-col h-full">
+      <span class="font-bold dark:text-gray-50">${project.title}</span>
+      <div class="block text-gray-500 dark:text-gray-300 flex-1"
+        >${project.description}
+      </div>
+      <div class="mt-3 dark:text-gray-200" id="Github link">
+        <a
+          href="${project.github}"
+          target="_blank"
+          class="hover:text-gray-600 text-4xl dark:hover:text-gray-400"
+        >
+          <i class="bi bi-github"></i>
+        </a>
+        ${
+          project?.url
+            ? `<a
+        href="${project.url}"
+        target="_blank"
+        class="hover:text-gray-600 text-4xl ml-1 dark:hover:text-gray-400"
+      >
+        <i class="bi bi-link-45deg"></i>
+      </a>`
+            : ''
+        }
+      </div>
+    </div>
+    <div id="badges" class="flex flex-wrap absolute top-0 ml-3 gap-2">
+  `;
+    project.badges.forEach((badge) => {
+      htmlText += `
+    <div class="badge ${badge.color} text-white">
+      <span class="mx-1">${badge.name}</span>
+    </div>
+  `;
+    });
+    htmlText += `</div></div>`;
+  });
+  htmlText += `</div></div>`;
+  return htmlText;
+}
+
+let htmlText = '';
+projectsList.forEach((projectSection, i) => {
+  htmlText += renderProjectSection(projectSection);
+  if (i !== projectsList.length - 1)
+    htmlText +=
+      '<hr class="w-full my-8 border-gray-200 dark:border-gray-700" />';
+});
+document.getElementById('list-projects').innerHTML = htmlText;
+
+// https://stackoverflow.com/questions/1125084/how-to-make-the-window-full-screen-with-javascript-stretching-all-over-the-scre
+// Experimental
+function requestFullScreen() {
+  // Supports most browsers and their versions.
+  var el = document.documentElement,
+    rfs = // for newer Webkit and Firefox
+      el.requestFullscreen ||
+      el.webkitRequestFullScreen ||
+      el.mozRequestFullScreen ||
+      el.msRequestFullscreen;
+  if (typeof rfs != 'undefined' && rfs) {
+    rfs.call(el);
+  } else if (typeof window.ActiveXObject != 'undefined') {
+    // for Internet Explorer
+    var wscript = new ActiveXObject('WScript.Shell');
+    if (wscript != null) {
+      wscript.SendKeys('{F11}');
+    }
+  }
+}
+
+requestFullScreen();
+
+document.addEventListener(
+  'keydown',
+  (e) => {
+    if (e.key === 'Enter') {
+      requestFullScreen(document.body);
+    }
+  },
+  false
+);
